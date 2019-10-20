9275276ab1f5aa1830e109a302e61e9737631d90
Mark Gallardo <mrk_gllrd@yahoo.com>
2019-10-19 22:24:25 -0500

    revised js file, addedajax to show gifs

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,63 +1,49 @@
-// initial array of Disney Characters
-var topics = ["Jafar", "Mickey Mouse", "Ariel", "Simba", "Buzz Lightyear", "Olaf", "Stich", "Genie", "Maleficent", "Aladdin"];
+// function that is to run upon page loading
+$(function() {
+  renderButtons(topics, 'searchButton', '#buttons-view');
+  console.log("Page Loaded");
+})
 
-$(".disneyCharacter").on("click", function() {
+  // initial array of Disney Characters
+  var topics = ["Jafar", "Mickey Mouse", "Ariel", "Simba", "Buzz Lightyear", "Olaf", "Stich", "Genie", "Maleficent", "Aladdin"];
 
-    var disneyChar = $(this).attr("data-name");
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + disneyChar + "&api_key=fUDAuKBG6mh5jPllypinr6PkAmcBBAkP";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(queryURL);
-
-      console.log(response);
-    });
-  });
-
-
-// Function for displaying movie data
-function renderButtons() {
-
-    // Deleting the movie buttons prior to adding new movie buttons
-    // (this is necessary otherwise we will have repeat buttons)
-    $("#buttons-view").empty();
-
-    // Looping through the array of movies
-    for (var i = 0; i < topics.length; i++) {
-
-      // Then dynamicaly generating buttons for each movie in the array.
-      // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-      var a = $("<button>");
-      // Adding a class
-      a.addClass("disneyCharacter");
-      // Adding a data-attribute with a value of the Disney Character at index i
-      a.attr("data-name", topics[i]);
-      // Providing the button's text with a value of the Disney Character at index i
+  function renderButtons(topics,classToAdd,areaToAddButtons){
+    // empties out the input area where new buttons are created
+    $(areaToAddButtons).empty();
+    for(var i = 0; i < topics.length; i++){
+      var a = $('<button>');
+      a.addClass(classToAdd);
+      a.attr('data-name', topics[i]);
       a.text(topics[i]);
-      // Adding the button to the HTML
-      $("#buttons-view").append(a);
+      $(areaToAddButtons).append(a);
     }
   }
 
-  // This function handles events where one button is clicked
-  $("#add-disney").on("click", function(event) {
-    // event.preventDefault() prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
-    event.preventDefault();
+  $(document).on('click','.searchButton', function(){
+    var disneyChar = $(this).data('name');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+disneyChar+"&api_key=fUDAuKBG6mh5jPllypinr6PkAmcBBAkP&limit=10";
 
-    // This line will grab the text from the input box
-    var disneyPerson = $("#character-input").val().trim();
-    // The movie from the textbox is then added to our array
-    topics.push(disneyPerson);
-
-    // calling renderButtons which handles the processing of our movie array
-    renderButtons();
-  });
-
-
-
-  renderButtons();
-
+    // fixed "ajax is not a function by adding uncompressed version of jquery instead of slim"
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response){
+        console.log(response);
+        for(var i = 0; i < response.data.length; i++){
+          var disneyDiv = $('<div class="searchResult">');
+          var rating = response.data[i].rating;
+          var p = $('<p>').text("Rating: "+ rating);
+          var animatedGif = response.data[i].images.fixed_height.url;
+          var stillGif = response.data[i].images.fixed_height_still.url;
+          var image = $('<img>');
+          image.attr('src',stillGif);
+          image.attr('data-still',stillGif);
+          image.attr('data-animated', animatedGif);
+          image.attr('data-state', 'still');
+          image.addClass('searchImage');
+          disneyDiv.append(p);
+          disneyDiv.append(image);
+          $('#disney-view').append(disneyDiv);
+        }
+    })
+  })
